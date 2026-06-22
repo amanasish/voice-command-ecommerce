83a89e213ed2a5468e447aa1d745fe37d5a2df95
AMAN ASISH GUPTA <amanasisgupta@gmail.com>
2026-06-22 18:36:29 +0530

    feat: complete rule-based NLP parser for MVP

---
+++ server/nlp/constants.js
@@ -0,0 +1,18 @@
+export const CATEGORIES = ["shirts", "jeans", "kurtas","phones"];
+
+export const COLORS = ["red", "blue", "black"];
+
+export const ACTIONS = {
+  filter: ["show", "find", "search"],
+  addToCart: ["add to cart"],
+  removeFromCart: ["remove last item"],
+  checkout: ["checkout", "place order"],
+  viewCart: ["show cart", "open cart", "view cart", "my cart"]
+};
+
+ 
+
+
+
+
+
---
+++ server/nlp/tests/parserTypes.test.js
@@ -0,0 +1,5 @@
+import { parse } from "../parserTypes.js";
+
+console.log(await parse("show me blue shirts under 1000"));
+
+console.log(await parse("checkout now"));
