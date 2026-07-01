1e005c971f7c4ca97e4bf94b2588eb7aba429a97
AMAN ASISH GUPTA <amanasisgupta@gmail.com>
2026-07-01 19:30:23 +0530

    Add login and register pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { CartProvider } from "./context/CartContext.jsx";
 import { ProductProvider } from "./context/ProductContext.jsx";
 import { VoiceProvider } from "./context/VoiceContext.jsx";
@@ -7,17 +7,24 @@ import CartDrawer from "./components/CartDrawer.jsx";
 import FeedbackBanner from "./components/FeedbackBanner.jsx";
 import Home from "./pages/Home.jsx";
 import Checkout from "./pages/Checkout.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 import "./index.css";
 
 function AppShell() {
+  const location = useLocation();
+  const hideNavbar = ["/login", "/register"].includes(location.pathname);
+
   return (
     <>
-      <Navbar />
+      {!hideNavbar && <Navbar />}
       <FeedbackBanner />
       <CartDrawer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
